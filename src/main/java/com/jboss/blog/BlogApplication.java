cffcbf4776207357e9b3173c64c0a524ad21bb9c
JBoss <abolade.akintomiwa@gmail.com>
2020-06-25 13:37:56 +0100

    Quick fix

--- src/main/java/com/jboss/blog/BlogApplication.java
+++ src/main/java/com/jboss/blog/BlogApplication.java
@@ -3,9 +3,10 @@ package com.jboss.blog;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-@EnableCaching
+@EnableJpaAuditing
 public class BlogApplication {
 
     public static void main(String[] args) {
